a52a112ac169b87da06b7eb2b316c2cd3067c175
PavzM <pavel_med1@outlook.com>
2020-11-09 14:29:37 -0600

    Primera actualizacion

--- proy.py
+++ proy.py
@@ -36,7 +36,7 @@ def AbrirArchivo():
             if ind > 0 and not (ind%2)==0:
                 tam.append(lin[ind] - (lin[ind-1]-1))
             ind = ind + 1
-    print("{0:11s} | {1:20s} | {2:25s} | {3:25s} | {4:20s} | {5:8s} | {6:10s} | {7:8s} | {8:4s} | {9:6s} | {10:6s} |".format("EMPLOYEE_ID","FIRST_NAME","LAST_NAME","EMAIL","PHONE_NUMBER","HIRE_DATE","JOB_ID","SALARY","COMMISSION_PCT","MANAGER_ID","DEPARTMENT_ID"))
+    #print("{0:11s} | {1:20s} | {2:25s} | {3:25s} | {4:20s} | {5:8s} | {6:10s} | {7:8s} | {8:4s} | {9:6s} | {10:6s} |".format("EMPLOYEE_ID","FIRST_NAME","LAST_NAME","EMAIL","PHONE_NUMBER","HIRE_DATE","JOB_ID","SALARY","COMMISSION_PCT","MANAGER_ID","DEPARTMENT_ID"))
     laCompleta = []
     while True:
         linea = archivo.readline().rstrip()#Debemos leer por número de caracteres
@@ -61,10 +61,75 @@ def AbrirArchivo():
         laCompleta.clear()
 
     archivo.close()
+#------------------------------------------------------------------------------------------
+def select():#forma de introducir elementos:>>select employee_id,first_name
+    listas = input(">> ").split()
+    sel = listas[1].split(',')
+    if listas[1] == "*":
+        pass
+    else:
+        for index in range(0,len(EMPLOYEE_ID)):
+            for el in sel:
+                if el.upper() == "EMPLOYEE_ID":
+                    if index==0:
+                        print("EMPLOYEE_ID | ")
+                    print(f"        {EMPLOYEE_ID[index]} | ", end='')
+                elif el.upper() == "FIRST_NAME":
+                    if index==0:
+                        print("EMPLOYEE_ID | ")
+                    print(f"{FIRST_NAME[index]} | ", end='')
+                elif el.upper() == "LAST_NAME":
+                    if index==0:
+                        print("EMPLOYEE_ID | ")
+                    print(f"{LAST_NAME[index]} | ", end='')
+                elif el.upper() == "EMAIL":
+                    if index==0:
+                        print("EMPLOYEE_ID | ")
+                    print(f"{EMAIL[index]} | ", end='')
+                elif el.upper() == "PHONE_NUMBER":
+                    if index==0:
+                        print("EMPLOYEE_ID | ")
+                    print(f"{PHONE_NUMBER[index]} | ", end='')
+                elif el.upper() == "HIRE_DATE":
+                    if index==0:
+                        print("EMPLOYEE_ID | ")
+                    print(f"{HIRE_DATE[index]} | ", end='')
+                elif el.upper() == "JOB_ID":
+                    if index==0:
+                        print("EMPLOYEE_ID | ")
+                    print(f"{JOB_ID[index]} | ", end='')
+                elif el.upper() == "SALARY":
+                    if index==0:
+                        print("EMPLOYEE_ID | ")
+                    print(f"{SALARY[index]} | ", end='')
+                elif el.upper() == "COMMISSION_PCT":
+                    if index==0:
+                        print("EMPLOYEE_ID | ")
+                    print(f"{COMMISSION_PCT[index]} | ", end='')
+                elif el.upper() == "MANAGER_ID":
+                    if index==0:
+                        print("EMPLOYEE_ID | ")
+                    print(f"{MANAGER_ID[index]} | ", end='')
+                elif el.upper() == "DEPARTMENT_ID":
+                    if index==0:
+                        print("EMPLOYEE_ID | ")
+                    print(f"{DEPARTMENT_ID[index]} | ", end='')
+            print()
+
+def where():
+    pass
+
+def diccionario():
+#yo
+    pass
+
 
 
 AbrirArchivo()
+#inst1 = input(">> ").split()
+#sel = inst1[1].split(',')
+#inst3 = input(">> ").split()
+select()
+where()
 
 #---------------------------------------------------------------------------------
-#for x in range(1,11):
-#    print ('{3}{0:2d}{3} {3}{1:3d}{3} {3}{2:4d}{3}'.format(x, x * x, x * x * x, '|'))
