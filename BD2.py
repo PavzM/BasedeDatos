063429af3f8d5d93598649ee7b90114647da7be0
AsaelEB <asaeleb@gmail.com>
2020-11-13 18:09:04 -0600

    Ya no sé que rayos hago, quitenme esto por favor.

--- BD2.py
+++ BD2.py
@@ -49,7 +49,7 @@ L_VAL = []
 
 def AbrirArchivo():
     """Esta función es para cargar la tabla desde un archivo"""
-    archivo = open('C:/Users/User/OneDrive/Escritorio/python-course/Progs/BasedeDatos/basededatos.txt','r')#C:/Users/User/OneDrive/Escritorio/python-course/Progs/BasedeDatos/basededatos.txt
+    archivo = open('','r')#C:/Users/User/OneDrive/Escritorio/python-course/Progs/BasedeDatos/basededatos.txt
     formato = archivo.readline().rstrip()
     formato = formato.split(',')
     lin = []
@@ -86,7 +86,7 @@ def AbrirArchivo():
     archivo.close()
 #------------------------------------------------------------------------------------------
 def Diccionario():
-    archivo = open('C:/Users/User/OneDrive/Escritorio/python-course/Progs/BasedeDatos/basededatos.txt','r')#C:/Users/User/OneDrive/Escritorio/python-course/Progs/BasedeDatos/basededatos.txt
+    archivo = open('','r')#C:/Users/User/OneDrive/Escritorio/python-course/Progs/BasedeDatos/basededatos.txt
     cadena = archivo.readline().rstrip()
     cadena= cadena.split(',')#aqui estan las palabras de los campos con las posiciones
     lin=[]
@@ -504,7 +504,7 @@ class TestFrame(wx.Frame):
         self.Close(True)
 
     def salir(self,event):#Salir
-        salir=wx.MessageDialog(None, 'Chaup :,(','Salir', style=wx.OK)
+        salir=wx.MessageDialog(None, 'Chaup :\'(','Salir', style=wx.OK)
         salir.ShowModal()
         self.Close(True)
         #-------------------------------------------------------------------
